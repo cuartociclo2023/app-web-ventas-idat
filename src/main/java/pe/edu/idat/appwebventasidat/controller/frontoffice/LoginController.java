5a7c13f065797a330427e38d8cd6843d10f22903
cuartociclo2023 <cuartocicloidat2023@gmail.com>
2023-11-09 21:59:28 -0500

    controlador login y registro

--- src/main/java/pe/edu/idat/appwebventasidat/controller/frontoffice/LoginController.java
+++ src/main/java/pe/edu/idat/appwebventasidat/controller/frontoffice/LoginController.java
@@ -1,4 +1,47 @@
 package pe.edu.idat.appwebventasidat.controller.frontoffice;
 
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.idat.appwebventasidat.model.bd.Usuario;
+import pe.edu.idat.appwebventasidat.service.UsuarioService;
+
+@AllArgsConstructor
+@Controller
+@RequestMapping("/auth")
 public class LoginController {
+
+    private UsuarioService usuarioService;
+
+    @GetMapping("/login")
+    public String login(){
+        return "frontoffice/auth/frmLogin";
+    }
+
+    @GetMapping("/registrar")
+    public String registrar(){
+        return "frontoffice/auth/frmRegistroUsuario";
+    }
+
+    @PostMapping("/guardarusuario")
+    public String guardarUsuario(@ModelAttribute Usuario usuario){
+        usuarioService.guardarUsuario(usuario);
+        return "frontoffice/auth/frmLogin";
+    }
+
+    @GetMapping("/login-success")
+    public String loginSuccess(){
+        return "redirect:/auth/dashboard";
+    }
+
+    @GetMapping("/dashboard")
+    public String dashboard(HttpServletRequest request){
+        return "frontoffice/auth/principal";
+    }
+
+
 }
